e6e4807d0210f3a35cd1af283acb021d89a367a7
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 12:58:31 +0400

    added

--- funtions.py
+++ funtions.py
@@ -7,3 +7,18 @@ def main():
     print("OPTION 3: MANAGE BOOKINGS")
     print("OPTION 4: STAFF LOGIN")
     print("OPTION 5: ABOUT")
+
+def NEW_BOOKING():
+    print("="*20, "NEW BOOKING", "="*20)
+
+def FLIGHT_STATUS():
+    print("="*20, "FLIGHT STATUS", "="*20)
+
+def MANAGE_BOOKINGS():
+    print("="*20, "MANAGE BOOKINGS", "="*20)
+
+def STAFF_LOGIN():
+    print("="*20, "STAFF LOGIN", "="*20)
+
+def ABOUT():
+    print("="*20, "ABOUT", "="*20)
