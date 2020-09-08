7da604ebd0e96fbf202770abaef4a2702332d3b1
NAVEED <imnaveed2003@gmail.com>
2020-09-08 13:57:39 +0400

    added

--- FLIGHT_SEATS.py
+++ FLIGHT_SEATS.py
@@ -1,4 +1,8 @@
 import pandas as pd
-list=[["","","","","",""],[1,2,"","",3,4]]
-df=pd.DataFrame(list,columns=["a","b","","","c","d"])
+list=[["1","1","","","1","1"]]
+a="0ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+indexx=[]
+for i in range(len(a)):
+    indexx.append(str(a[i]))
+df=pd.DataFrame(list,columns=["1","2","","","3","4"],index=indexx)
 print(df)
