80eaf7a7559f2233f6d7531dbae1f5333fc1f777
NAVEED <imnaveed2003@gmail.com>
2020-09-08 12:52:37 +0400

    Added

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ from funtions import *
 main()
 response=input("ENTER OPTION NUMBER: ")
 if response=="1":
-<<<<<<< HEAD
-    
-=======
     pass
->>>>>>> a2b6809a43adcfd382cacc6d4e4ed69b6b786cfd
 
     
 
