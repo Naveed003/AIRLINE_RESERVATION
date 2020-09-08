e05aacf37e7fc5fa5693ff69cf19bf39f1371c98
NAVEED <imnaveed2003@gmail.com>
2020-09-08 12:11:47 +0400

    added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from datetime import date
+import sqlite3
 import pandas as pd
+import time
+import random
+from random import randint
+from datetime import datetime
+import mysql.connector
+mydb=mysql.connector.connect(host="remotemysql.com",user="QxKi8MQlUR",passwd="Kf0GcKV5sh",port=3306,database="QxKi8MQlUR")
+mycursor=mydb.cursor()
