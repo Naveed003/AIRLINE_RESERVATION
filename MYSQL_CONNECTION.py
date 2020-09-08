a2b6809a43adcfd382cacc6d4e4ed69b6b786cfd
NAVEED <imnaveed2003@gmail.com>
2020-09-08 12:36:07 +0400

    added

--- MYSQL_CONNECTION.py
+++ MYSQL_CONNECTION.py
@@ -2,3 +2,12 @@ import mysql.connector
 
 mydb=mysql.connector.connect(host="remotemysql.com",user="QxKi8MQlUR",passwd="Kf0GcKV5sh",port=3306,database="QxKi8MQlUR")
 mycursor=mydb.cursor()
+
+
+print(__name__)
+
+def hello():
+    print(__name__)
+
+
+
---
+++ funtions.py
@@ -0,0 +1,9 @@
+def main():
+    print("="*20, "GIHS AIRLINE", "="*20)
+    print("\n", "="*8, "MAIN MENU", "="*8, "\n")
+
+    print("OPTION 1: NEW BOOKING")
+    print("OPTION 2: FLIGHT STATUS")
+    print("OPTION 3: MANAGE BOOKINGS")
+    print("OPTION 4: STAFF LOGIN")
+    print("OPTION 5: ABOUT")
