25e9769baa95cf361585163562b42162a5b6bb7b
BuildTools <unconfigured@null.spigotmc.org>
2020-09-09 18:48:13 +0400

    added

--- ROUTE_GENERATOR.py
+++ ROUTE_GENERATOR.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import random
-
+from flight_id import *
 
 COUNTRY1 = "DXB"
 COUNTRY2 = input("ENTER COUNTRY 2: ").upper()
 COUNTRY3 = input("ENTER COUNTRY 3: ").upper()
-
-flight_id1 = "G"+str(random.randint(100, 999))
-flight_id2 = "G"+str(random.randint(100, 999))
-flight_id3 = "G"+str(random.randint(100, 999))
+F_n(B)
+flight_id1 = "G"+str(B)
+F_n(B)
+flight_id2 = "G"+str(B)
+F_n(B)
+flight_id3 = "G"+str(B)
 list = [
     ["FLIGHT_NO", "ORIGIN", "DESTINATION"],
     [flight_id1, COUNTRY1, COUNTRY2],
@@ -22,4 +24,3 @@ list = [
 ]
 df = pd.DataFrame(list[1:], columns=list[0])
 print(df)
-
--- flight_id.py
+++ flight_id.py
@@ -1,8 +1,17 @@
-
-import random
-a="G"
-list=[]
-for i in range(25):
-  B=random.randint(100,999)
-  list.append(a+str(B))
-  print(a+str(B))
+def F_n(B):
+    global B
+    import random
+    import json
+    with open('flight_Numbers.txt', 'r') as f:
+        flight_Numbers = json.loads(f.read())
+    print(flight_Numbers)
+    while True:
+        B=random.randint(100,999)
+        if B in flight_Numbers:
+            print(B)
+            pass
+        else:
+            flight_Numbers.append(B)
+            with open('flight_Numbers.txt', 'w') as f:
+                f.write(json.dumps(flight_Numbers))
+            break
