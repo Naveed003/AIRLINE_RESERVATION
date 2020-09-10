b131b9e34e743c29e9e5954c4e20816c15c80936
NAVEED WINDOWS <imnaveed2003@gmail.com>
2020-09-10 23:09:36 +0400

    added

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import random
 from random import randint
 from datetime import datetime
 import mysql.connector
+mydb=mysql.connector.connect(host="remotemysql.com",user="QxKi8MQlUR",passwd="Kf0GcKV5sh",port=3306,database="QxKi8MQlUR")
+mycursor=mydb.cursor()
+
 
 
 
@@ -103,7 +106,7 @@ def NEW_BOOKING():
 
 
     def flights_extract():
-        
+        pass
                 
 
 
