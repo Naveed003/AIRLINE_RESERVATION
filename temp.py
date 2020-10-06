8bb4e0f2ed4fecf8835c95dee55834aaebc655b4
NAVEED <imnaveed2003@gmail.com>
2020-10-06 11:13:30 +0400

    addeed

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-import pandas as pd
+""" import pandas as pd
 import sys
 
 list = [[1, 2, 3], [7, 8, 9]]
@@ -11,4 +11,6 @@ pp_details=[['12345678', 'NAVE', 4866]]
 pp_detail=pd.DataFrame(pp_details,columns=["PASSPORT NUMBER","NAME","PNR"])
 
 text="helglo "
-print("\u0332".join(text))
+print("\u0332".join(text)) """
+
+1,4,5,6,12
